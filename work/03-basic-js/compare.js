c4de6513e34c590249df7ab57d475f43094942e8
Ian OU <oyjf012@gmail.com>
2024-09-25 01:10:04 -0700

    Completes 03-basic-js assignment

--- work/03-basic-js/compare.js
+++ work/03-basic-js/compare.js
@@ -2,9 +2,30 @@
 /* DO NOT MODIFY EXCEPT WHERE ALLOWED */
 module.exports = compare; // DO NOT MODIFY - USED FOR TESTING
 
-function compare( word, guess ) {  // DO NOT MODIFY
+function compare(word, guess) {
+  // DO NOT MODIFY
 
-/* YOU MAY MODIFY THE LINES BELOW */
+  /* YOU MAY MODIFY THE LINES BELOW */
 
-  return 0; // this line is wrong
+  const wordCount = {};
+  const guessCount = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i].toUpperCase();
+    wordCount[letter] = (wordCount[letter] || 0) + 1;
+  }
+
+  for (let i = 0; i < guess.length; i++) {
+    const letter = guess[i].toUpperCase();
+    guessCount[letter] = (guessCount[letter] || 0) + 1;
+  }
+
+  let matchCount = 0;
+  for (const letter in wordCount) {
+    if (guessCount[letter]) {
+      matchCount += Math.min(wordCount[letter], guessCount[letter]);
+    }
+  }
+
+  return matchCount;
 }
